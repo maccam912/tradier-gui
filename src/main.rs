b591acda138c100b03cdc1f8a88dbbc6b41d49cf
maccam912 <maccam912@gmail.com>
2021-08-11 08:01:13 -0500

    updateds

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{sync::Mutex, thread};
 
 use eframe::{egui, epi};
-use once_cell::sync::Lazy;
 use eyre::Result;
+use once_cell::sync::Lazy;
 
 #[derive(Clone, Copy)]
 struct State {
@@ -11,9 +11,7 @@ struct State {
 
 unsafe impl Send for State {}
 
-static STATE: Lazy<Mutex<State>> = Lazy::new(|| {
-    Mutex::new(State{balance: 0.0})
-});
+static STATE: Lazy<Mutex<State>> = Lazy::new(|| Mutex::new(State { balance: 0.0 }));
 
 struct TradierApp {
     state: &'static Mutex<State>,
@@ -21,9 +19,7 @@ struct TradierApp {
 
 impl Default for TradierApp {
     fn default() -> Self {
-        Self {
-            state: &STATE,
-        }
+        Self { state: &STATE }
     }
 }
 
@@ -35,13 +31,26 @@ impl epi::App for TradierApp {
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut epi::Frame<'_>) {
         let Self { state } = self;
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Tradier Application");
-            ui.horizontal(|ui| {
-                ui.label("Balance: ");
-                ui.label(format!("{:?}", state.lock().unwrap().balance));
+        egui::TopBottomPanel::top("header_panel").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.heading("Tradier Application");
             });
+        });
 
+        egui::TopBottomPanel::bottom("footer_panel").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.heading(format!("Balance: {}", state.lock().unwrap().balance));
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                if ui.button("Update").clicked() {
+                    thread::spawn(|| {
+                        let _ = update();
+                    });
+                }
+            })
         });
 
         frame.set_window_size(ctx.used_size());
@@ -50,7 +59,10 @@ impl epi::App for TradierApp {
 
 fn update() -> Result<()> {
     let acct = tradier::account::get_user_profile::get_user_profile().unwrap();
-    let balance = tradier::account::get_balances::get_balances(acct.profile.account[0].account_number.clone()).unwrap();
+    let balance = tradier::account::get_balances::get_balances(
+        acct.profile.account[0].account_number.clone(),
+    )
+    .unwrap();
     let bal = balance.balances.total_equity;
     STATE.lock().unwrap().balance = bal;
     Ok(())
@@ -58,12 +70,6 @@ fn update() -> Result<()> {
 
 fn main() {
     let options = eframe::NativeOptions::default();
-
-    thread::spawn(
-        || {
-            let _ = update();
-        }
-    );
 
     eframe::run_native(Box::new(TradierApp::default()), options);
 }
